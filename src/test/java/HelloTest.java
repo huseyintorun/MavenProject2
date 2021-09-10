6c414e068da09854eec337b5c5a43d19cf1d7422
huseyintorun <torunhsyn1989@icloud.com>
2021-09-10 08:52:42 +0200

    Fourth Commit

--- src/test/java/HelloTest.java
+++ src/test/java/HelloTest.java
@@ -2,5 +2,6 @@ public class HelloTest {
     public static void main(String[] args) {
         System.out.println("Hallo Test");
         System.out.println("GitHub remoote Repo is already there");
+        System.out.println("More Job");
     }
 }
